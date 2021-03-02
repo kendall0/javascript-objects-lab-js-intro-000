dd5a8778e20583b0633ef26aa74e042d925bfda5
Kendall George <kendallanngeorge@gmail.com>
2021-03-02 17:27:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var recipes = {prop: 1, prop2: 2};
 
+
+
 function updateObjectWithKeyAndValue(object, key, value) {
-  recipes[key] = 'new value';
+  var obj = { prop: 1 };
   return recipes;
 }
+
+
