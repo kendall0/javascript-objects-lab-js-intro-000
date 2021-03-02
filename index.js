5281e36743170d0fa9f37537289d092b5d0d2078
Kendall George <kendallanngeorge@gmail.com>
2021-03-02 16:54:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,15 +3,3 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   reuturn recipes;
 }
-
-var obj = { foo: 'bar' }
-
-var newObj = Object.assign({}, obj)
-
-newObj // { foo: 'bar' }
-
-delete newObj.foo // true
-
-newObj // {}
-
-obj // { foo: 'bar' }
